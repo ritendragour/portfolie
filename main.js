019b7c23b0132119864ab5ae58854016779d326a
Ritendra <ritendra.gour@clickspurr.com>
2023-02-12 23:55:16 +0530

    .

--- main.js
+++ main.js
@@ -64,3 +64,18 @@ function sendEmail() {
             alert("mail sent successfully")
         });
 }
+
+// Required google recaptcha logic
+document.getElementById("googleRecaptcha").addEventListener("submit", function (evt) {
+
+    var response = grecaptcha.getResponse();
+    if (response.length == 0) {
+        //reCaptcha not verified
+        alert("please verify you are humann!");
+        evt.preventDefault();
+        return false;
+    }
+    //captcha verified
+    //do the rest of your validations here
+
+});
